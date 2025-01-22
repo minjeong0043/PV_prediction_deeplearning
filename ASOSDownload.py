e98e60248947e7447f64b07246017f5aa51076c6
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2025-01-22 14:56:51 +0900

    Add files via upload

---
+++ ASOSDownload.py
@@ -0,0 +1,177 @@
+import os.path
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import  ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import  Service
+from selenium.webdriver.support.ui import  WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def file_down(driver):
+    # 조회 및 다운
+    # region_all = driver.find_element(By.XPATH, '//*[@id="ztree_1_check"]').click()
+    # time.sleep(1)
+    # option_all = driver.find_element(By.XPATH, '//*[@id="ztree1_1_check"]').click()
+    # time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="dsForm"]/div[3]/button').click()
+    # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.LINK_TEXT, '속초(90)')))
+    # time.sleep(50)
+    WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+    # wait = WebDriverWait(driver, 15)  # 최대 10초 대기//*[@id="wrap_content"]/div[4]/div[1]/div/a[2]
+
+    Excel = driver.find_element(By.XPATH, '//*[@id="wrap_content"]/div[4]/div[1]/div/a[2]')
+    driver.execute_script("arguments[0].scrollIntoView();", Excel)
+    time.sleep(1)
+    Excel.click()
+    time.sleep(1)
+    option = driver.find_element(By.XPATH, '//*[@id="reqstPurposeCd7"]')
+    driver.execute_script("arguments[0].scrollIntoView();", option)
+    time.sleep(1)
+    # option.click()
+    action = ActionChains(driver)
+    time.sleep(1)
+    action.move_to_element(option).click().perform()
+    action = ActionChains(driver)
+    action.move_to_element(option).click().perform()
+    action = ActionChains(driver)
+    action.move_to_element(option).click().perform()
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="wrap-datapop"]/div/div[2]/div/a[2]').click()
+    # time.sleep(50)
+    WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+
+def file_down_iter(i):
+    download_dir = 'D:\mj Kim\진행중인 업무\종관기상관측(ASOS)\month1'
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+    options = webdriver.ChromeOptions()
+    prefs = {
+        "download.default_directory": download_dir,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True
+    }
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=options)
+
+    driver.get("https://data.kma.go.kr/data/grnd/selectAsosRltmList.do?pgmNo=36")
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="loginBtn"]').click()
+    WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.ID, 'loading-')))
+    try:
+        driver.find_element(By.ID, 'loginId').send_keys("0043kmj@gmail.com")
+        driver.find_element(By.ID, 'passwordNo').send_keys("muxinnrmmg")
+        driver.find_element(By.ID, 'loginbtn').click()
+        selenium_cookies = driver.get_cookies()
+        cookies = {cookie['name']: cookie['value'] for cookie in selenium_cookies}
+
+        time.sleep(1)
+        driver.find_element(By.XPATH, '//*[@id="dataFormCd"]/option[5]').click()
+        time.sleep(1)
+        start_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[1]/button/img').click()
+        time.sleep(1)
+        driver.find_element(By.XPATH, '//*[@id="datepicker_year"]').click()
+        time.sleep(1)
+        set_year = driver.find_element(By.XPATH, '//*[@id="datepicker_year"]/option[24]').click()
+        time.sleep(1)
+        # set_month = driver.find_element(By.XPATH,
+        #                                 '//*[@id="datepicker_month"]/option[1]').click()  # //*[@id="datepicker_month"]/option[12] 12월임
+        # time.sleep(2)
+        set_day = driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/table/tbody/tr[1]/td[4]/a').click()
+        time.sleep(1)
+        end_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[2]/button/img').click()
+        time.sleep(1)
+        set_year = driver.find_element(By.XPATH, '//*[@id="datepicker_year"]/option[24]').click()
+        time.sleep(1)
+        # set_month = driver.find_element(By.XPATH, '//*[@id="datepicker_month"]/option[1]').click()
+        # time.sleep(2)
+        set_day = driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/table/tbody/tr[1]/td[4]/a').click()
+        time.sleep(1)
+        # option 선택
+        driver.find_element(By.XPATH, '//*[@id="ztree_1_check"]').click()
+        driver.find_element(By.XPATH, '//*[@id="ztree1_1_check"]').click()
+
+        if i < 8:
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            start_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[1]/button/img').click()
+            set_day = driver.find_element(By.XPATH,f'//*[@id="ui-datepicker-div"]/table/tbody/tr[1]/td[{i}]/a').click()
+            end_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[2]/button/img').click()
+            set_day = driver.find_element(By.XPATH,f'//*[@id="ui-datepicker-div"]/table/tbody/tr[1]/td[{i}]/a').click()
+            # #option 선택
+            # driver.find_element(By.XPATH, '//*[@id="ztree_1_check"]').click()
+            # driver.find_element(By.XPATH, '//*[@id="ztree1_1_check"]').click()
+            # 조회 및 다운
+            file_down(driver)
+            print(f"day : {i-3} / 31")
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+        elif i < 15:
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            start_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[1]/button/img').click()
+            set_day = driver.find_element(By.XPATH, f'//*[@id="ui-datepicker-div"]/table/tbody/tr[2]/td[{i-7}]/a').click()
+            end_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[2]/button/img').click()
+            set_day = driver.find_element(By.XPATH, f'//*[@id="ui-datepicker-div"]/table/tbody/tr[2]/td[{i-7}]/a').click()
+
+            # 조회 및 다운
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            file_down(driver)
+            print(f"day : {i-3} / 31")
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+        elif i < 22:
+            start_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[1]/button/img').click()
+            set_day = driver.find_element(By.XPATH,f'//*[@id="ui-datepicker-div"]/table/tbody/tr[3]/td[{i - 14}]/a').click()
+            end_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[2]/button/img').click()
+            set_day = driver.find_element(By.XPATH,f'//*[@id="ui-datepicker-div"]/table/tbody/tr[3]/td[{i - 14}]/a').click()
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            # 조회 및 다운
+            file_down(driver)
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            print(f"day : {i-3} / 31")
+        elif i < 29:
+            start_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[1]/button/img').click()
+            set_day = driver.find_element(By.XPATH,
+                                          f'//*[@id="ui-datepicker-div"]/table/tbody/tr[4]/td[{i - 21}]/a').click()
+            end_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[2]/button/img').click()
+            set_day = driver.find_element(By.XPATH,
+                                          f'//*[@id="ui-datepicker-div"]/table/tbody/tr[4]/td[{i - 21}]/a').click()
+
+            # 조회 및 다운
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            file_down(driver)
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            print(f"day : {i-3} / 31")
+        else:
+            start_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[1]/button/img').click()
+            set_day = driver.find_element(By.XPATH,
+                                          f'//*[@id="ui-datepicker-div"]/table/tbody/tr[5]/td[{i - 28}]/a').click()
+            end_yyyymmdd = driver.find_element(By.XPATH, '//*[@id="dayData"]/dd/div[2]/button/img').click()
+            set_day = driver.find_element(By.XPATH,
+                                          f'//*[@id="ui-datepicker-div"]/table/tbody/tr[5]/td[{i - 28}]/a').click()
+
+            # 조회 및 다운
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            file_down(driver)
+            WebDriverWait(driver, 50).until(EC.invisibility_of_element_located((By.ID, "loading-mask")))
+            print(f"day : {i-3} / 31")
+    except Exception as e:
+        print(f"error: {e}")
+
+    finally:
+        time.sleep(10)
+        driver.quit()
+
+# download_dir = 'D:\mj Kim\진행중인 업무\종관기상관측(ASOS)\month1'
+# if not os.path.exists(download_dir):
+#     os.makedirs(download_dir)
+# options = webdriver.ChromeOptions()
+# prefs = {
+#     "download.default_directory": download_dir,
+#     "download.prompt_for_download": False,
+#     "download.directory_upgrade": True,
+#     "safebrowsing.enabled":True
+# }
+# options.add_experimental_option("prefs", prefs)
+# driver = webdriver.Chrome(options= options)
+
+for i in range(1+3, 31+3):
+    file_down_iter(i)
