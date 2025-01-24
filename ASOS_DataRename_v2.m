ef3e5d11c9788ef074c33f905f421737614a5743
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2025-01-24 23:35:59 +0900

    Update and rename ASOS_DataRename.m to ASOS_DataRename_v2.m

---
+++ ASOS_DataRename_v2.m
@@ -0,0 +1,61 @@
+clc; clear all; close all;
+function file_Rename_inMonth(month_path)
+    files = fullfile(month_path, '*.xls*');
+    files = dir(files);
+    for i = 1:length(files)
+    oldFileName = files(i).name;
+    oldFilePath = fullfile(month_path, oldFileName);
+    data = readtable(oldFilePath, 'FileType','text');
+
+    time = [data{1, 3}];
+    y = year(time(:));
+    m = month(time(:));
+    d = day(time(:));
+
+    newFileName = oldFileName(1:12) +string(y) + sprintf('%02d', m) + sprintf('%02d', d) + '.xls';
+    newFilePath = fullfile(month_path, newFileName);
+    status = movefile(oldFilePath, newFilePath);
+
+    if status 
+        disp(['Success : ',oldFileName,' -->',newFileName]); 
+    else
+        disp(['Failed : ',oldFileName]);
+    end
+    end
+end
+
+
+% folder = 'D:\mj Kim\진행중인 업무\종관기상관측(ASOS)_rawdata\ASOS2020';
+% monthFolder = fullfile(folder, 'month*');
+% monthFolder = dir(monthFolder);
+% 
+% for i = 1:12
+%     % month = str2num(monthFolder(1).name(6:7));
+%     month = str2num(regexprep(monthFolder(i).name, '[^0-9]', ''));
+%     disp(['month : ', num2str(month)]);
+%     month_path = fullfile(folder, monthFolder(i).name);
+%     file_Rename_inMonth(month_path)
+% end
+
+
+folder = '\\Ugrid\sml_nas\연구용데이터\종관기상관측(ASOS)';
+yearFolder = fullfile(folder, 'ASOS*');
+yearFolder = dir(yearFolder);
+
+for y = 1:4
+    year = str2num(regexprep(yearFolder(y).name, '[^0-9]', ''))
+    disp([' Year : ', num2str(year)]);
+    year_path = fullfile(folder, yearFolder(y).name);
+    
+    % month
+    monthFolder = fullfile(year_path, 'month*');
+    monthFolder = dir(monthFolder);
+
+    for m = 1:12
+        month = str2num(regexprep(monthFolder(m).name, '[^0-9]', ''));
+        disp(['month : ', num2str(month)]);
+        month_path = fullfile(year_path, monthFolder(m).name);
+        file_Rename_inMonth(month_path);
+    end
+end
+
