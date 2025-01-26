8d3f899e6010457e80cebf8ccc5231c1e2af4d4b
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2025-01-27 00:00:44 +0900

    Update ASOS_DataRename_ChangeTimeStep_v3.m

--- ASOS_DataRename_v2.m
+++ ASOS_DataRename_v2.m
@@ -24,6 +24,65 @@ function file_Rename_inMonth(month_path)
     end
 end
 
+function oneFile_changeTimestep(file_path)
+    [folder, file_name, ext] = fileparts(file_path);
+    data = readtable(file_path, 'FileType', 'text');
+
+    % startTime = datetime('2020-01-01 00:00', 'Format', 'HH:mm');
+    % endTime = datetime('2020-01-01 23:50', 'Format', 'HH:mm');
+    startTime = datetime('00:00', 'Format', 'HH:mm');
+    endTime = datetime('23:50', 'Format', 'HH:mm');
+
+
+    setTime = startTime:minutes(10):endTime;
+    setTime_str = datestr(setTime, 'HH:MM');
+    setTime_str(1,:) = '00:01';
+
+    n = length(data{:,3});
+    new_data = {};
+    timeIndex = 1;
+    dataIndex = 1;
+
+    for i = 1:n
+        data_time = datetime(data{i,3}, 'Format', 'HH:mm');
+        data_time_str = datestr(data_time, 'HH:MM');
+        if data_time_str == setTime_str(timeIndex, :)
+            disp(sprintf('data processing . . . (dataIndex : %d) - %s', dataIndex, regexprep(file_name, '[^0-9]', '')));
+            new_data(dataIndex, :) = table2cell(data(i, :));
+            dataIndex = dataIndex + 1;
+            timeIndex = timeIndex + 1;
+            if timeIndex > length(setTime_str)
+                timeIndex = 1;
+            end
+        end
+    end
+
+    header = {'지점', '지점명', '일시', '기온(°C)', '기온 QC플래그', '누적강수량(mm)', '풍향(deg)',...
+    '풍향 QC플래그', '풍속(m/s)', '풍속 QC플래그', '현지기압(hPa)', '현지기압 QC플래그', '해면기압(hPa)',...
+    '해면기압 QC플래그', '습도(%)', '습도 QC플래그', '일사(MJ/m^2)', '일조(Sec)'};
+    new_data_table = cell2table(new_data, 'variableNames', header);
+
+    output_file = fullfile(folder, [file_name+ '_TimeStep10'+ '.xls'])
+    disp(['Output file path: '+ output_file]);
+    writetable(new_data_table, output_file);
+    disp(['TimeStep10 data saved to '+ output_file])
+
+    if exist(file_path, 'file') == 2
+        delete(file_path);
+        disp(['Original file deleted '+ file_path]);
+    else
+        disp('Original file not found');
+    end
+end
+
+
+function month_changeTimestep(month_path)
+    files = fullfile(month_path, '*.xls*');
+    files = dir(files);
+    for i = 1:length(files)
+        oneFile_changeTimestep(file_path)
+    end
+end
 
 % folder = 'D:\mj Kim\진행중인 업무\종관기상관측(ASOS)_rawdata\ASOS2020';
 % monthFolder = fullfile(folder, 'month*');
@@ -37,6 +96,7 @@ end
 %     file_Rename_inMonth(month_path)
 % end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Main %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 folder = '\\Ugrid\sml_nas\연구용데이터\종관기상관측(ASOS)';
 yearFolder = fullfile(folder, 'ASOS*');
@@ -55,7 +115,9 @@ for y = 1:4
         month = str2num(regexprep(monthFolder(m).name, '[^0-9]', ''));
         disp(['month : ', num2str(month)]);
         month_path = fullfile(year_path, monthFolder(m).name);
-        file_Rename_inMonth(month_path);
+        %file_Rename_inMonth(month_path);
+        month_changeTimestep(month_path);
+        
     end
 end
 
