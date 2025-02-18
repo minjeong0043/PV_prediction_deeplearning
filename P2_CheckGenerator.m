eb140ab2ea6596998fb568e16b9df15952c8101f
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2025-02-18 14:13:34 +0900

    Update P2_CheckGenerator.m

--- P2_CheckGenerator.m
+++ P2_CheckGenerator.m
@@ -1,112 +1,35 @@
 clc; clear all; close all;
-load('dlnetGenerator_ir(inputSize128, latentvector256, dongAsia, Epoch50).mat', 'dlnetGenerator');
-dlnetGenerator_inputSize128 = dlnetGenerator;
-load('dlnetGenerator_ir(inputSize256, latentvector256, dongAsia, Epoch100).mat', 'dlnetGenerator');
-dlnetGenerator_inputSize256 = dlnetGenerator;
-clear dlnetGenerator
 
-% landom Latent vector
+load('model_checkpoint_Generator_Epoch2.mat', 'dlnetGenerator');
+
+% prepare random latent vector
 numLatentInputs = 256;
-z = randn(1, 1, numLatentInputs, 144, 'single');
+z = randn(1,1,numLatentInputs, 25, 'single'); % latent vector 25개
 dlZ = dlarray(z, 'SSCB');
-dlXGenerated = predict(dlnetGenerator_inputSize128, dlZ);
-dlX = extractdata(dlXGenerated);
-size(dlX(:,:,:,1));
+dlXGenerated = predict(dlnetGenerator, z);
+dlXGenerated_dlX = predict(dlnetGenerator, dlZ);
 
-% 단일 이미지 출력
-% h = figure;
-% imshow(dlX(:,:,:,1))
-
-% 타일 이미지 출력
+% Plot generated images
 h = figure;
-I = imtile(dlX);
-I_rescale = rescale(I);
-image(I_rescale);
-title('Generated Images');
-
-% outputFolder = '';
-% saveas(h, fullfile(outputFolder, 'CheckingGenerator.png'));
-saveas(h, 'CheckingGenerator.png');
-close(h)
-
-% Plot Latent vector space
-z_forPlot = squeeze(z)
-size(z_forPlot(:,1))
-k = figure;
-scatter(1:256, z_forPlot(:,1), "filled")
-xlim([0, 256])
-size(squeeze(z))
-
-
-p = figure;
-img_index = 1;
-z_index = 1;
-for i = 1:12*24
-   subplot(12,24, i);
-   if mod(i, 2) == 1
-       imshow(dlX(:,:,:,img_index))
-       axis off
-       title(['z_index : ', num2str(img_index)])
-   else
-       scatter(1:256, z_forPlot(:,1), "filled")
-       title(['img index : ', num2str(img_index)])
-       img_index = img_index + 1;
-   end
-end
-
-p = figure;
-
-numRows = 12; % 서브플롯 행 개수
-numCols = 24; % 서브플롯 열 개수 (12x2 = 한 세트)
-totalPlots = numRows * numCols; % 총 서브플롯 개수
-
-img_index = 1;
-
-for i = 1:totalPlots
-   subplot(numRows, numCols, i);
-   
-   if mod(i, 2) == 1  % 홀수 번째는 imshow
-       imshow(dlX(:,:,:,img_index));
-       title(['Image Index: ', num2str(img_index)]);
-   else  % 짝수 번째는 scatter
-       scatter(1:256, z_forPlot(:,img_index), "filled");
-       title(['Scatter Index: ', num2str(img_index)]);
-       img_index = img_index + 1; % 한 세트가 끝날 때만 증가
-   end
-end
-
-
-img = figure;
-for i = 1:12*12
-    subplot(12, 12, i);
-    imshow(dlX(:,:,:,i));
-    axis off
-end
-
-z = figure;
-for i = 1:12*12
-    subplot(12, 12, i);
-    scatter(1:256, z_forPlot(:,i), "filled");
-end
-
-
-montage(dlX, 'Size', [12 12]);
-
-z = figure;
-tiledlayout(12,12, 'TileSpacing', 'none', 'Padding', 'none');
-for i = 1:12*12
-    nexttile;
-    scatter(1:256, z_forPlot(:,i), "filled");
-    axis off;
-end
-
-
-%% 
-f = figure;
-f.Position = [950, 670, 560, 560];
+h.Position = [950, 670, 560, 560];
 tLay = tiledlayout(5, 5, "TileSpacing", "none", "Padding", "none");
-imshow(z_forPlot)
+I = imtile(rescale(dlXGenerated));
+image(I);
 xticklabels([]);
 yticklabels([]);
 axis tight;
 axis off;
+
+% Plot random latent vector
+p = figure;
+set(p, 'Position', [950, 670, 560, 560]);
+
+tLay = tiledlayout(5, 5, 'TileSpacing', 'none', 'Padding', 'none');
+
+I = squeeze(z); % (256×25)로 변환
+size(I)
+for i = 1:25
+    subplot(5, 5, i);
+    scatter(1:256, I(:,i), "filled");
+    xlim([1,256]);
+end
