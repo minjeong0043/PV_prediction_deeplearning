c89719384b6332306e45cb7fb9ac5e513b0ee0d3
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2025-07-01 17:46:47 +0900

    Update winds_folder_250701.py

--- winds_folder_250701.py
+++ winds_folder_250701.py
@@ -6,13 +6,14 @@ import os
 from PIL import Image
 import pandas as pd
 
-folder_path = r'D:\천리안2호_적외(구름상)_2022~2023_10min_crop'
+folder_path = r'\\Ugrid\sml_nas\연구용데이터\태풍이미지\1.카눈'
 file_list = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')]) # 폴더 내 파일 리스트
 # print(file_list)
 
 # 고산 지역 좌표 (33.294, 126.163)-->(1509.54, 1621.61)
 gosan_x = int(1509.54)
-gosan_y = int(1595.5) #이미지 전처리 고려한 좌표값 산출
+# gosan_y = int(1595.5) #이미지 전처리 고려한 좌표값 산출
+gosan_y = int(1621.61)
 # 합천 지역 좌표 (35.565, 128.17) -->(1596.78, 1495.79)
 hapcheon_x = int(1596.78)
 hapcheon_y = int(1466.6)
@@ -30,22 +31,21 @@ index = np.arange(2,len(file_list)-1)
 # print(index)
 wind_speed_excel = []
 wind_dir_excel = []
-# for i in range(len(file_list) - 2):
-for i in range(5):
+for i in range(len(file_list) - 2):
+# for i in range(5):
     files_window = file_list[i:i+3]
-    # print(f"window { i} : {files_window}")
     file_path1 = os.path.join(folder_path, file_list[i])
     file_path2 = os.path.join(folder_path, file_list[i+1])
     file_path3 = os.path.join(folder_path, file_list[i+2])
 
-    wind_speed, wind_dir = WindSpeedDir(file_path1, file_path2, file_path3)
-    wind_speed_excel.append(wind_speed[gosan_x, gosan_y])
-    wind_dir_excel.append(wind_dir[gosan_x, gosan_y])
-    print(f"wind Speed : {wind_speed[gosan_x, gosan_y]:.2f}, wind_dir : {wind_dir[gosan_x, gosan_y]:.2f}\n")
+    wind_speed, wind_dir = WindSpeedDir(file_path1, file_path2, file_path3, gosan_x, gosan_y)
+    wind_speed_excel.append(wind_speed)
+    wind_dir_excel.append(wind_dir)
+    print(f"(index {i}) wind Speed : {wind_speed} [m/s], wind_dir : {wind_dir} [deg]\n")
 
 # excel로 저장
 df1 = pd.DataFrame(index, columns=['Index'])
-df2 = pd.DataFrame(wind_speed_excel, columns=['Speed'])
-df3 = pd.DataFrame(wind_dir_excel, columns=['Direction'])
+df2 = pd.DataFrame(wind_speed_excel, columns=['Speed [m/s]'])
+df3 = pd.DataFrame(wind_dir_excel, columns=['Direction [deg]'])
 merged_df = pd.concat([df1, df2, df3], axis=1)
-merged_df.to_excel('wind_from_satelliteImage.xlsx', sheet_name = 'WindData')
+merged_df.to_excel('wind_from_satelliteImage_inGosan_카눈.xlsx', sheet_name = 'WindData')
